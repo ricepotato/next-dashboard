09d86ec43c74edc9885af3f814e595d81c341084
ricepotato <sukjun40@naver.com>
2023-11-01 12:41:43 +0900

    fix routes error

---
+++ app/api/customers/route.ts
@@ -0,0 +1,11 @@
+export async function GET() {
+  const data = {};
+
+  return Response.json({ data });
+}
+
+export async function POST() {
+  const data = {};
+
+  return Response.json({ data });
+}
